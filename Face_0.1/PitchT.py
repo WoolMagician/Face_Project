a5c42891b7ff1d500392e466328fceb955809a93
WoolMagician <francesco.podda.db@gmail.com>
2016-03-09 14:35:31 +0100

    Added files via upload

--- Face_0.1/PitchT.py
+++ Face_0.1/PitchT.py
@@ -1,33 +1,50 @@
-from __future__ import division
-from numpy.fft import rfft
-from scikits.audiolab import flacread
-from numpy import argmax, sqrt, mean, diff, log
-from scipy.signal import blackmanharris
-from time import time
-import sys
+# Read in a WAV and find the freq's
+import pyaudio
+import wave
+import numpy as np
 
-from parabolic import parabolic
+chunk = 2048
 
+# open up a wave
+wf = wave.open('test-tones/440hz.wav', 'rb')
+swidth = wf.getsampwidth()
+RATE = wf.getframerate()
+# use a Blackman window
+window = np.blackman(chunk)
+# open stream
+p = pyaudio.PyAudio()
+stream = p.open(format =
+                p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = RATE,
+                output = True)
 
-def freq_from_fft(sig, fs):
-    """Estimate frequency from peak of FFT
-    
-    """
-    # Compute Fourier transform of windowed signal
-    windowed = sig * blackmanharris(len(sig))
-    f = rfft(windowed)
-    
-    # Find the peak and interpolate to get a more accurate peak
-    i = argmax(abs(f)) # Just use this for less-accurate, naive version
-    true_i = parabolic(log(abs(f)), i)[0]
-    
-    # Convert to equivalent frequency 
-    return fs * true_i / len(windowed)
-
-filename = sys.argv[1]
-
-print 'Reading file "%s"\n' % filename
-signal, fs, enc = flacread(filename)
-
-print 'Calculating frequency from FFT:',
-print (freq_from_fft(signal, fs))
+# read some data
+data = wf.readframes(chunk)
+# play stream and find the frequency of each chunk
+while len(data) == chunk*swidth:
+    # write data out to the audio stream
+    stream.write(data)
+    # unpack the data and times by the hamming window
+    indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth),\
+                                         data))*window
+    # Take the fft and square each value
+    fftData=abs(np.fft.rfft(indata))**2
+    # find the maximum
+    which = fftData[1:].argmax() + 1
+    # use quadratic interpolation around the max
+    if which != len(fftData)-1:
+        y0,y1,y2 = np.log(fftData[which-1:which+2:])
+        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+        # find the frequency and output it
+        thefreq = (which+x1)*RATE/chunk
+        print "The freq is %f Hz." % (thefreq)
+    else:
+        thefreq = which*RATE/chunk
+        print "The freq is %f Hz." % (thefreq)
+    # read some more data
+    data = wf.readframes(chunk)
+if data:
+    stream.write(data)
+stream.close()
+p.terminate()
